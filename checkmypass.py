31af59ae422c309cc370239dcf2353590bc62328
Chris <cjmugs@gmail.com>
2021-07-13 16:11:32 -0500

    updates

--- checkmypass.py
+++ checkmypass.py
@@ -3,7 +3,7 @@ import hashlib
 import sys
 
 def request_api_data(query_char):
-      url = 'https://api.pwnedpasswords.com/range/' + query_char
+  url = 'https://api.pwnedpasswords.com/range/' + query_char
   res = requests.get(url)
   if res.status_code != 200:
     raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
